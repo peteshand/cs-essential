c81db7d5aa829ce3d93f80108b5ae0a74c713bc3
P.J.Shand <pete@peteshand.net>
2019-05-13 08:27:02 +1000

    Add Console Trace static function

---
+++ trace/Trace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+public static class Trace
+{
+    public static void trace(string message)
+    {
+        // get call stack
+        StackTrace stackTrace = new StackTrace();
+
+        string className = stackTrace.GetFrame(1).GetMethod().DeclaringType.Name;
+
+        // get calling method name
+        Console.WriteLine(className + ": " + message);
+
+        //Console.WriteLine(message);
+    }
+}
