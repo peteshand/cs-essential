d9c0c72b20de91feda29d1e1f157fd56828c790a
P.J.Shand <pete@peteshand.net>
2019-05-13 08:27:20 +1000

    Add Notifier

---
+++ notifier/Notifier.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using signals;
+
+namespace notifier
+{
+    public class Notifier<T> : Signal1<T>
+    {
+        public bool requireChange = true;
+
+        private T _value;
+        private string id;
+
+        public T Value
+        {
+            get
+            {
+                return this._value;
+            }
+            set
+            {
+                //Console.WriteLine("SET");
+                //this._value = value;
+
+                if (!changeRequired(value)) return;
+                this._value = value;
+                this.Dispatch(value);
+            }
+        }
+
+        private bool changeRequired(T value)
+        {
+            return !EqualityComparer<T>.Default.Equals(_value, value) || !requireChange;
+        }
+
+        public Notifier()
+        {
+
+        }
+
+        public Notifier(T defaultValue, string id = "", bool fireOnAdd = false) 
+        {
+            this._value = defaultValue;
+            this.id = id;
+        }
+    }
+}
